076e863ad6d816fde1b6b9af961fb1fc0b1ddf13
hui huang <huanghui@zdns.cn>
2018-04-07 02:53:09 +0800

    add menu

--- shijiasan.py
+++ shijiasan.py
@@ -1,15 +1,36 @@
+# -*- coding: utf-8 -*-
+
 from Tkinter import *
-class Application(Frame):
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.pack()
+class Application(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        self.title("103")
+        self.geometry("500x500+600+200")
+        self.createMenu()
         self.createWidgets()
 
+    def hello(self):
+        print("xxx")
+
+    def createMenu(self):
+        self.menuBar = Menu(master = self)
+        self.filemenu = Menu(self.menuBar, tearoff = 0)
+        self.filemenu.add_command(label = "导出key", command = self.hello)
+        self.filemenu.add_command(label = "设置问题", command = self.hello)
+        self.filemenu.add_command(label = "关闭", command = self.quit)
+        self.menuBar.add_cascade(label = "操作", menu = self.filemenu)
+
+        self.helpmenu = Menu(self.menuBar, tearoff = 0)
+        self.helpmenu.add_command(label = "支持", command = self.hello)
+        self.helpmenu.add_command(label = "联系", command = self.hello)
+        self.helpmenu.add_command(label = "关于", command = self.hello)
+        self.menuBar.add_cascade(label = "帮助", menu = self.helpmenu)
+
+        self.config(menu = self.menuBar)
+        
     def createWidgets(self):
-        self.helloLabel = Label(self, text='Hello, world!')
-        self.helloLabel.pack()
-        self.quitButton = Button(self, text='Quit', command=self.quit)
-        self.quitButton.pack()
+        pass
+        
+
 app = Application()
-app.master.title('Hello World')
 app.mainloop()
